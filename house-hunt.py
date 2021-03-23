de386b41e02b648666a5dc033b170a23d702cf74
Aaron Ogan <aaron.ogan@gmail.com>
2021-03-23 09:02:51 -0500

    Testing realtor.com free api

--- house-hunt.py
+++ house-hunt.py
@@ -1,4 +1,50 @@
+import requests
 import yaml
+from abc import ABC, abstractmethod
+
+class RapidApi:
+    def __init__(self, config):
+        self.config = config
+
+    def get(self, endpoint_url, headers, params):
+        response = requests.request("GET", endpoint_url, headers=headers, params=params)
+
+        if response.status_code == 200:
+            return response.json()
+        else:
+            raise Exception("There was a problem retrieving data", endpoint_url, headers, params)
+
+
+class RealEstateApi:
+    def __init__(self):
+        pass
+
+    @abstractmethod
+    def get_listings(self):
+        raise NotImplementedError
+
+
+class RealtorFreeRealEstateApi(RapidApi, RealEstateApi):
+    def __init__(self, config):
+        RapidApi.__init__(self, config)
+        RealEstateApi.__init__(self)
+
+    def get_listings(self):
+        base_url = self.config['base-url']
+        endpoint_url = ''.join((base_url, "properties"))
+
+        headers = self.config['headers']
+
+        querystring = {
+            "city": "Denver",
+            "state_code": "CO",
+            "offset": "0",
+            "limit": "15"
+        }
+
+        json_response = self.get(endpoint_url, headers, querystring)
+        return json_response
+
 
 def get_config():
     file_name = "config.yml"
@@ -10,5 +56,11 @@ def get_config():
                 return None
 
 if __name__ == "__main__":
-    print(get_config())
+    config = get_config()
+
+    realtor_free_config = config['apis']['rapidapi']['realtor-free']
+    realtor_api = RealtorFreeRealEstateApi(realtor_free_config)
+
+    print(realtor_api.get_listings())
+
 
